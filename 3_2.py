ecb04d9cc11b8b4fb462059dec5dc2aa50287646
John Hodges <git@jhodges.net>
2025-09-10 14:31:26 -0600

    Rewritten to use quit() in except

--- 3_2.py
+++ 3_2.py
@@ -1,16 +1,14 @@
 hours = input("Enter Hours:")
+rate = input("Enter Rate:")
 try :
     h = float(hours)
-    try :
-        rate = input("Enter Rate:")
-        r = float(rate)
-        if h > 40.0 :
-            ot = h - 40.0
-            pay = ot * r * 1.5 + 40.0 * r
-        else :
-            pay = h * r
-        print (pay)
-    except :
-        print("Error, please enter numeric input")
+    r = float(rate)
 except :
     print("Error, please enter numeric input")
+    quit() # learned quit() command.
+if h > 40.0 :
+    ot = h - 40.0
+    pay = ot * r * 1.5 + 40.0 * r
+else :
+    pay = h * r
+print (pay)
