31af2c58af614b63215fbd261d469a9dfc4c79bf
John Hodges <git@jhodges.net>
2025-09-13 11:51:03 -0600

    Attempted to pare down code

--- 9_4/9_4.py
+++ 9_4/9_4.py
@@ -1,23 +1,25 @@
-#fname = input('Enter file name: ')
-fname = 'mbox-short.txt'
+fname = input('Enter file name: ')
+if len(fname) < 1 : fname = 'mbox-short.txt'
 handle = open(fname)
 senders = dict()
 emails = list()
-for line in handle :
-    if not line.startswith('From ') : continue
-    words = line.split()
-    emails.append(words[1])
+
+# Add email addresses to list
+for x in handle :
+    if not x.startswith('From ') : continue
+    x = x.rstrip()
+    emails.append(x.split()[1])
+
+# Add email addresses to dictionary
 for email in emails :
     senders[email] = senders.get(email, 0) + 1
 
-bigsender = None
-sentcount = None
-for key,value in senders.items() : # was missing .items()
-    if bigsender == None :
-        bigsender = key
-        sentcount = value
-    else :
-        if value > sentcount :
-            sentcount = value
-            bigsender = key
-print(bigsender,sentcount)
+# Find highest frequency sender
+mailer = None
+highcount = None
+for x,y in senders.items() :
+    if mailer is None or y > highcount :
+        mailer = x
+        highcount = y
+        
+print(mailer,highcount)
--- lec_9_2.py
+++ lec_9_2.py
@@ -3,3 +3,7 @@ names = ['csev', 'cwen', 'csev', 'zqian', 'cwen']
 for name in names :
     counts[name] = counts.get(name, 0) + 1
 print(counts)
+
+# .get(key, defaultvalue)
+    # if key is in dictionary, return the value,
+    # else, add key and set value to defaultvalue
