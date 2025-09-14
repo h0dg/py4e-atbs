b57eba7be4b2c0421e5633a0de96301c4b48d831
John Hodges <git@jhodges.net>
2025-09-13 20:07:26 -0600

    Updated to include list comprehension challenge
    condensed code to 2 lines to practice list comprehension syntax.

--- assignment_11.py
+++ assignment_11.py
@@ -1,13 +1,28 @@
+#import re
+
+#file = open('regex_sum_2294092.txt')
+#nums = list()
+#for line in file :
+#    tmp = re.findall('[0-9]+', line)
+#    if len(tmp) == 0 : 
+#        continue    # skip lines without numbers
+#    for num in tmp :
+#        num = int(num) # convert to integer
+#        nums.append(num)
+
+#print(sum(nums))
+
 import re
+print(sum([int(x) for x in re.findall('[0-9]+', open('regex_sum_2294092.txt').read())]))
 
-file = open('regex_sum_2294092.txt')
-nums = list()
-for line in file :
-    tmp = re.findall('[0-9]+', line)
-    if len(tmp) == 0 : 
-        continue    # skip lines without numbers
-    for num in tmp :
-        num = int(num) # convert to integer
-        nums.append(num)
+# 1. open('filename.txt').read()
+#   - opens the file and reads the file into a single string.
+#
+# 2. re.findall('[0-9]+', ... ) where '...' is text to search
+# 
+# 3. [int(x) for x in ...]
+#   - loops through each x in the list of regex matches.
+#   - int(x) converts the string to an integer.
 
-print(sum(nums))
+# LIST COMPREHENSION:
+# [EXPRESSION for ITEM in ITERABLE]
